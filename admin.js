a32e7508be287ffc49a093904c04722076bcbdb2
APEX-nxs <apexnexusweb3@gmail.com>
2025-09-18 09:16:25 +0100

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+import { DONATION_CONTRACT_ADDRESS, DONATION_CONTRACT_ABI } from "./config";
+
+export async function rescueTokenAdmin(tokenAddress, recipientAddress) {
+  if (!window.ethereum) throw new Error("No wallet");
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const chain = await provider.getNetwork();
+  const contract = new ethers.Contract(DONATION_CONTRACT_ADDRESS[chain.chainId], DONATION_CONTRACT_ABI, signer);
+  const tx = await contract.rescueToken(tokenAddress, recipientAddress);
+  await tx.wait();
+  return tx.hash;
+}
