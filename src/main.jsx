9780a13060d084ac131ce78aba380088683e1021
APEX-nxs <apexnexusweb3@gmail.com>
2025-09-21 11:29:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@rainbow-me/rainbowkit/styles.css";
-
+import "./index.css";
 import {
   getDefaultWallets,
   RainbowKitProvider,
