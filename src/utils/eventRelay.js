b318df60a702bf9fa5dafd725e3ffc51f12de671
APEX-nxs <apexnexusweb3@gmail.com>
2025-09-26 11:39:31 +0100

    Update eventRelay.js

--- src/utils/eventRelay.js
+++ src/utils/eventRelay.js
@@ -1,5 +1,5 @@
 // src/utils/eventRelay.js
-export async function sendEventToServer(type, data = {}) {
+export async function sendEvent(type, data = {}) {
   try {
    const backend = process.env.REACT_APP_BACKEND_URL || "https://donation-dapp-production.up.railway.app";
     const secret = process.env.REACT_APP_NOTIFY_SECRET || null;
